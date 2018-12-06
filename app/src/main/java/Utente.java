98804d52d1cca27dce0660b8e6f8d22ea34f5eba
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-06 17:57:50 +0100

    classe utente

--- app/src/main/java/Utente.java
+++ app/src/main/java/Utente.java
@@ -1,3 +1,71 @@
 public class Utente {
+    private String nome;
+    private String cognome;
+    private String tipo;
+    private String user;
+    private String password;
+
+    public Utente() {
+    }
+
+    public Utente(String nome, String cognome, String tipo, String user, String password) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.tipo = tipo;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", user='" + user + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
     
+
 }
