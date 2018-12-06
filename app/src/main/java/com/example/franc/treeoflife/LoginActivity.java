ca93805d4f4b724f1221acc4ca53eaaa7d03e5af
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-06 18:38:32 +0100

    classe utente

--- app/src/main/java/com/example/franc/treeoflife/LoginActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/LoginActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 
 public class LoginActivity extends Activity {
 
