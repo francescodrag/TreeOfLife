9c36094b747242917b790f9a5c1bc34946da42c3
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-07 15:07:52 +0100

    foto bimbo e inizio activiti per adozione

--- app/src/main/java/com/example/franc/treeoflife/LeMieAdozioniActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/LeMieAdozioniActivity.java
@@ -2,12 +2,21 @@ package com.example.franc.treeoflife;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LeMieAdozioniActivity extends Activity {
+    Button inviaDoni,organizzaIncontro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_le_mie_adozioni);
+        setupViews();
+    }
+
+    private void setupViews() {
+        inviaDoni=findViewById(R.id.InviaDoni_Adozione);
+        organizzaIncontro=findViewById(R.id.OrganizzaIncontro_Adozioni);
+
     }
 }
