a680791e7d2ac2407557fb4f2d1226d1233f8fb6
francescodrag <francescodragone@live.it>
2018-12-07 11:06:35 +0100

    prova

--- app/src/main/java/com/example/franc/treeoflife/HomeActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/HomeActivity.java
@@ -4,16 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
+
+    Utente utente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        getUtente();
+
+        Toast.makeText(HomeActivity.this, "Benvenuto, "+utente.getNome()+" "+utente.getCognome()+".", Toast.LENGTH_LONG).show();
+
         }
 
-
+        private void getUtente(){
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            if(extras!=null)
+                utente = (Utente) extras.getSerializable("Utente");
+        }
     }
 
--- app/src/main/java/com/example/franc/treeoflife/LoginActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.franc.treeoflife;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
@@ -12,6 +14,7 @@ public class LoginActivity extends Activity {
 
     TextView Username, Password;
     Button Login;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,14 @@ public class LoginActivity extends Activity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+
+                try {
+                    login();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                progressDialog.dismiss();
             }
         });
 
@@ -33,35 +43,38 @@ public class LoginActivity extends Activity {
         Username = findViewById(R.id.username_Login);
         Password = findViewById(R.id.password_Login);
         Login = findViewById(R.id.login_Login);
+        progressDialog = new ProgressDialog(LoginActivity.this);
     }
 
-    private void login() {
+    private void login() throws InterruptedException {
+
+        progressDialog.setMessage("Login in corso...");
+        progressDialog.show();
+
+
         if (Username.getText().toString().toUpperCase().equals("MISSIONARIO") || Password.getText().toString().toUpperCase().equals("MISSIONARIO"))
         {
             Utente missionario = new Utente("Pino", "Mauro", "M");//M=missionario
-            Intent intent = new Intent(this, HomeActivity.class);
-            Bundle extras = new Bundle();
-            extras.putSerializable("Utente",missionario);
-            intent.putExtras(extras);
-            startActivity(intent);
+            inviaUtente(missionario);
         }
         else if (Username.getText().toString().toUpperCase().equals("SOSTENITORE") || Password.getText().toString().toUpperCase().equals("SOSTENITORE"))
         {
             Utente sostenitore = new Utente("Claudio", "Cassese", "S");//S=sostenitore a distanza
-            Intent intent = new Intent(this, HomeActivity.class);
-            Bundle extras = new Bundle();
-            extras.putSerializable("Utente",sostenitore);
-            intent.putExtras(extras);
-            startActivity(intent);
+            inviaUtente(sostenitore);
         }
         else if (Username.getText().toString().toUpperCase().equals("VOLONTARIO") || Password.getText().toString().toUpperCase().equals("VOLONTARIO"))
         {
             Utente volontario = new Utente("Francesco", "Napolitano", "V");//V=sostenitore volontario
-            Intent intent = new Intent(this, HomeActivity.class);
-            Bundle extras = new Bundle();
-            extras.putSerializable("Utente",volontario);
-            intent.putExtras(extras);
-            startActivity(intent);
+            inviaUtente(volontario);
         }
+    }
+
+    private void inviaUtente(Utente utente) throws InterruptedException {
+        Intent intent = new Intent(this, HomeActivity.class);
+        Bundle extras = new Bundle();
+        extras.putSerializable("Utente",utente);
+        intent.putExtras(extras);
+        //Thread.sleep(2000);
+        startActivity(intent);
     }
 }
