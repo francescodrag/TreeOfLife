eb49b7fb2b156e3d24171c68b1cc06a6de3bcd4b
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-07 16:03:07 +0100

    nuova classe lavora con noi

---
+++ app/src/main/java/com/example/franc/treeoflife/LavoraConNoiActivity.java
@@ -0,0 +1,13 @@
+package com.example.franc.treeoflife;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LavoraConNoiActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lavora_con_noi);
+    }
+}
