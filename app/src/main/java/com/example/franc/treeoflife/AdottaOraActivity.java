99204c6ac44d86d36308b3cff5e3d053daba0636
francescodrag <francescodragone@live.it>
2018-12-07 15:11:24 +0100

    07/12/18 ore 15:11

---
+++ app/src/main/java/com/example/franc/treeoflife/AdottaOraActivity.java
@@ -0,0 +1,14 @@
+package com.example.franc.treeoflife;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdottaOraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adotta_ora);
+
+    }
+}
