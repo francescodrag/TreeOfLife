7ed9a4361119af0c768d23fc6382b6d8ab35e3a2
francescodrag <francescodragone@live.it>
2018-12-08 10:07:22 +0100

    Task InviaDoni Completato Task LavoraConNoi Completato Task OrganizzaIncontro Completato

--- app/src/main/java/com/example/franc/treeoflife/LavoraConNoiActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/LavoraConNoiActivity.java
@@ -1,18 +1,25 @@
 package com.example.franc.treeoflife;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LavoraConNoiActivity extends Activity {
 
     Button HomePage, DiventaVolontario, AdottaOra;
+    TextView Nome, Cognome, Eta, Motivo;
     ImageView Area, Logo;
     Utente utente;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +63,10 @@ public class LavoraConNoiActivity extends Activity {
         DiventaVolontario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(Nome.getText().toString().equals("")||Cognome.getText().toString().equals("")||Eta.getText().toString().equals("")||Motivo.getText().toString().equals(""))
+                    Toast.makeText(LavoraConNoiActivity.this, "Compila tutti i campi, per favore.", Toast.LENGTH_SHORT).show();
+                else
+                    allert();
             }
         });
     }
@@ -67,6 +77,10 @@ public class LavoraConNoiActivity extends Activity {
         AdottaOra = findViewById(R.id.adotta_LavoraConNoi);
         Area = findViewById(R.id.area_LavoraConNoi);
         Logo = findViewById(R.id.logo_LavoraConNoi);
+        Nome = findViewById(R.id.nome_LavoraConNoi);
+        Cognome = findViewById(R.id.cognome_LavoraConNoi);
+        Eta = findViewById(R.id.eta_LavoraConNoi);
+        Motivo = findViewById(R.id.motivo_LavoraConNoi);
     }
     private void getUtente(){
         Intent intent = getIntent();
@@ -80,5 +94,31 @@ public class LavoraConNoiActivity extends Activity {
         extras.putSerializable("Utente",utente);
         intent.putExtras(extras);
         startActivity(intent);
+    }
+
+    private void allert(){
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(LavoraConNoiActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(LavoraConNoiActivity.this);
+        }
+        builder.setTitle("Attenzione!")
+                .setMessage("Sei sicuro di voler inviare la tua canditatura?")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(LavoraConNoiActivity.this, "Sarai contattato a breve. Keep In touch", Toast.LENGTH_LONG).show();
+                        intent=new Intent(LavoraConNoiActivity.this, HomeActivity.class);
+                        inviaUtente(utente);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        intent=new Intent(LavoraConNoiActivity.this, HomeActivity.class);
+                        inviaUtente(utente);
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
--- app/src/main/java/com/example/franc/treeoflife/OrganizzaIncontroActivity.java
+++ app/src/main/java/com/example/franc/treeoflife/OrganizzaIncontroActivity.java
@@ -1,17 +1,24 @@
 package com.example.franc.treeoflife;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrganizzaIncontroActivity extends Activity {
-    Button HomePage, LavoraConNoi, AdottaOra;
+    Button HomePage, LavoraConNoi, AdottaOra, OrganizzaIncontro;
+    TextView Periodo, Luogo;
     ImageView Area, Logo;
     Utente utente;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +65,16 @@ public class OrganizzaIncontroActivity extends Activity {
                 inviaUtente(utente);
             }
         });
+
+        OrganizzaIncontro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Periodo.getText().toString().equals("")||Luogo.getText().toString().equals(""))
+                    Toast.makeText(OrganizzaIncontroActivity.this, "Compila tutti i campi, per favore.", Toast.LENGTH_SHORT).show();
+                else
+                    allert();
+            }
+        });
     }
 
     private void setupView() {
@@ -66,6 +83,9 @@ public class OrganizzaIncontroActivity extends Activity {
         AdottaOra = findViewById(R.id.adotta_Home);
         Area = findViewById(R.id.area_Home);
         Logo = findViewById(R.id.logo_Home);
+        Periodo = findViewById(R.id.periodo_OrganizzaIncontro);
+        Luogo = findViewById(R.id.luogo_OrganizzaIncontro);
+        OrganizzaIncontro = findViewById(R.id.organizzaIncontro_OrganizzaIncontro);
     }
 
     private void getUtente(){
@@ -80,5 +100,31 @@ public class OrganizzaIncontroActivity extends Activity {
         extras.putSerializable("Utente",utente);
         intent.putExtras(extras);
         startActivity(intent);
+    }
+
+    private void allert(){
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(OrganizzaIncontroActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(OrganizzaIncontroActivity.this);
+        }
+        builder.setTitle("Attenzione!")
+                .setMessage("Sei sicuro di voler organizzare l'incontro?")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(OrganizzaIncontroActivity.this, "Ti abbiamo inviato un email con i dettagli", Toast.LENGTH_LONG).show();
+                        intent=new Intent(OrganizzaIncontroActivity.this, HomeActivity.class);
+                        inviaUtente(utente);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        intent=new Intent(OrganizzaIncontroActivity.this, HomeActivity.class);
+                        inviaUtente(utente);
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
